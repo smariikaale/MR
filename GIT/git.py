d9f3d69ba5e56bc35ce208f69924abe24b73b939
Smarika <smarikaale82@gmail.com>
2025-09-08 17:46:32 +0545

    git.py changed

--- GIT/git.py
+++ GIT/git.py
@@ -4,10 +4,10 @@
 #Git Flow:
 #git init -initializes the git(one time for a project)
 #git add file_name-adds the content to git //git add .-adds all untracted files
-#commit changes-git commit ~m "message"
-#push content to githuh-git push ~u origin branch_name=>~u is used in first push
-#~u-upstream=>tracks branch_name for next push
-#git push origin branch_name//to find what's in ~u
+#commit changes-git commit -m "message"
+#push content to githuh-git push -u origin branch_name=>-u is used in first push
+#-u=>upstream=>tracks branch_name for next push
+#git push origin branch_name//to find what's in -u
 #U-untracted
 #A-added to git
 #M-modified
